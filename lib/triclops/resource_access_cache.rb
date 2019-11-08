7c09f3454a1643128b3738480a4eb9a0cd416991
Eric O <eric@ejdigitalmedia.com>
2019-11-08 11:01:03 -0500

    Add resource access cache as a low latency way of keeping track of recently accessed resources

---
+++ lib/triclops/resource_access_cache.rb
@@ -0,0 +1,44 @@
+module Triclops
+  # A class that keeps track of recently accessed resource ids so that they can
+  # be retrieved in batch and persisted long-term to the Resource persistence layer.
+  class ResourceAccessCache
+    # Singleton pattern Triclops::ResourceAccessCache instance for this class.
+    def self.instance
+      @instance ||= new(
+        Redis.new(
+          host: REDIS_CONFIG[:host],
+          port: REDIS_CONFIG[:port],
+          password: REDIS_CONFIG[:password],
+          thread_safe: true
+        ),
+        "#{Rails.application.class.module_parent_name}:#{Rails.env}:raster_access_entries".freeze
+      )
+    end
+
+    # Initialize a new Triclops::ResourceAccessCache object.
+    # @param redis [Redis] A Redis connection instance.
+    # @param cache_list_key [String] A key to use for the cache list entry.
+    def initialize(redis, cache_list_key)
+      @redis = redis
+      @cache_list_key = cache_list_key
+    end
+
+    # Adds the given identifier to the set of cached identifiers. Multiple calls
+    # with the same identifier will only result in the identifier being added once.
+    # @param identifier [String] An identifier
+    def add(identifier)
+      @redis.sadd(@cache_list_key, identifier)
+    end
+
+    # Returns all cached identifiers (with no guarantees about order).
+    # @return [Array<String>] An array of string identifiers.
+    def all
+      @redis.smembers(@cache_list_key)
+    end
+
+    # Clears all cached identifiers.
+    def clear
+      @redis.del(@cache_list_key)
+    end
+  end
+end
---
+++ spec/triclops/resource_access_cache_spec.rb
@@ -0,0 +1,81 @@
+require 'rails_helper'
+
+RSpec.describe Triclops::ResourceAccessCache do
+  let(:cache_list_key) { "#{Rails.application.class.module_parent_name}:#{Rails.env}:raster_access_entries".freeze }
+  let(:instance) do
+    described_class.new(
+      Redis.new(
+        host: REDIS_CONFIG[:host],
+        port: REDIS_CONFIG[:port],
+        password: REDIS_CONFIG[:password],
+        thread_safe: true
+      ),
+      cache_list_key
+    )
+  end
+
+  context ".instance" do
+    it "creates and returns a singleton of the correct type" do
+      expect(described_class.instance).to be_a(described_class)
+    end
+
+    it "returns the same object instance when called multiple times" do
+      obj1 = described_class.instance
+      obj2 = described_class.instance
+      expect(obj1).to equal(obj2)
+    end
+  end
+
+  context '#initialize' do
+    it 'successfully creates a new instance and sets up appropriate instance variables' do
+      expect(instance).to be_a(described_class)
+      expect(instance.instance_variable_get('@redis')).to be_a(Redis)
+      expect(instance.instance_variable_get('@cache_list_key')).to eq(cache_list_key)
+    end
+  end
+
+  context '#add', redis: true do
+    before {
+      instance.clear
+    }
+
+    it 'successfully adds entries' do
+      instance.add('abc')
+      expect(instance.all).to eq(['abc'])
+      instance.add('xyz')
+      expect(instance.all).to eq(['abc', 'xyz'])
+    end
+  end
+
+  context '#all', redis: true do
+    before {
+      instance.clear
+    }
+
+    it 'returns all added entries' do
+      expect(instance.all).to eq([])
+      instance.add('a')
+      expect(instance.all.sort).to eq(['a'])
+      instance.add('b')
+      expect(instance.all.sort).to eq(['a', 'b'])
+    end
+  end
+
+  context '#clear', redis: true do
+    before {
+      instance.clear
+    }
+
+    it 'clears all added entries' do
+      instance.add('a')
+      expect(instance.all.sort).to eq(['a'])
+      instance.clear
+      expect(instance.all).to eq([])
+      instance.add('b')
+      instance.add('c')
+      expect(instance.all.sort).to eq(['b', 'c'])
+      instance.clear
+      expect(instance.all).to eq([])
+    end
+  end
+end
