257fa10e15401f99e981a004f88c581f4f2ebb00
Eric O <eric@ejdigitalmedia.com>
2022-05-31 15:09:37 -0400

    Re-add ensure_json_request to force .json ending in API requests (and reject .html), at least for now

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -5,6 +5,14 @@ class ApiController < ActionController::API
 
   private
 
+    # Returns 406 status if format requested is not json. This method can be
+    # used as a before_action callback for any controllers that only respond
+    # to json.
+    def ensure_json_request
+      return if request.format.blank? || request.format == :json
+      head :not_acceptable
+    end
+
     # Generates JSON with errors
     #
     # @param String|Array json response describing errors
